fe69f7baffa6336043f3ca200874e795d7d074f2
Bieelogrom <gabrielamorim15436@gmail.com>
2025-02-05 12:04:23 -0300

    feat: criação de pasta de fotos

--- app.py
+++ app.py
@@ -5,6 +5,20 @@ import datetime, time
 import os, sys
 from threading import Thread
 
+global capture,rec_frame,grey,switch,neg,face,rec,out
+capture=0
+grey=0
+neg=0
+face=0
+switch=1
+rec=0
+
+#Cria a pasta para armazenar as fotos salvas
+try:
+    os.mkdir('./fotos')
+except OSError as error:
+    pass
+
 app = Flask(__name__, template_folder="./templates")
 
 @app.route("/")
