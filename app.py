8e8f0cd6540a2a03cdfe9fe5d1b42737995d2543
Bieelogrom <gabrielamorim15436@gmail.com>
2025-02-05 12:01:35 -0300

    feat: imports de numpy e opencv

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response, request
+import numpy as np
+import cv2
+import datetime, time
+import os, sys
+from threading import Thread
 
 app = Flask(__name__, template_folder="./templates")
 
