f598f81f693cbc514b1ef0e4afe10b2e4eeb4cc5
Bieelogrom <gabrielamorim15436@gmail.com>
2025-02-06 12:54:28 -0300

    feat: add routes for ligar and desligar, update index.html with forms, and remove stop.html

--- app.py
+++ app.py
@@ -1,11 +1,5 @@
 from flask import Flask, render_template, Response, request
-import numpy as np
 import cv2
-import datetime, time
-import os, sys
-from threading import Thread
-
-
 
 app = Flask(__name__, template_folder="./templates")
 
@@ -13,6 +7,16 @@ app = Flask(__name__, template_folder="./templates")
 def home():
     return render_template('index.html')
 
+@app.route("/ligar",methods=['POST'])
+def ligar():
+    return render_template('ligar.html')
+
+
+@app.route("/desligar",methods=['POST'])
+def desligar():
+    return render_template('desligar.html')       
+ 
+        
 if __name__ == "__main__":
     app.run(debug=True)
     
